005a5dc89406256a8d481edfb4a00e23cf5fde5f
Daniel Garcia <dagarcia@e1z2r4p3.42.us.org>
2019-09-06 16:10:50 -0700

    part 1

---
+++ setting.py
@@ -0,0 +1,19 @@
+#info
+TITLE = "asdf"
+WIDTH = 700
+HEIGHT = 500
+FPS = 60
+FONT_NAME = 'arial'
+HS_FILE = "highscore.txt"
+
+# Player properties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
